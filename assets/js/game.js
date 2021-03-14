8f46b665212d7417bd1057f3558fb0f5978cb2ba
nikol411@hotmail.com <nikol411@hotmail.com>
2021-03-14 14:06:47 -0400

    prompt error bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,7 @@ var startGame = function() {
      if (playerInfo.health > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+        
 
         // pick new enemy to fight based on the index of the enemyNames array
         var pickedEnemyObj = enemyInfo[i];
@@ -28,8 +29,9 @@ var startGame = function() {
     }
 
     // after loop ends, we are either out of playerInfo.health or enemies to fight, so run the endGame function
-    endGame();
+endGame();
 };
+
 
 // function to end game
 var endGame = function() {
@@ -174,8 +176,21 @@ var randomNumber = function(min, max) {
 };
 
 // player information
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null){
+        name = prompt("What is your robot's name?")
+    }
+
+    console.log("Your robot's name is "," + name");
+    return name;
+};
+
 var playerInfo = {
     name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
